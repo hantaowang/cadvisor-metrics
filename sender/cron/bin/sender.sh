9dfd277fd4fbae9be5d202584d78ace3f1dca1fc
Will Wang <hwang97@berkeley.edu>
2017-04-29 20:21:06 -0700

    removed unused files

--- sender/cron/bin/sender.sh
+++
@@ -1,1 +0,0 @@
-python /sender-setup.py
--- sender/cron/start.sh
+++
@@ -1,4 +0,0 @@
-echo "export CADVISOR_IPS=$CADVISOR_IPS" > /cron/cronfile
-mkdir /cron/bin/Cron
-ln /cron/bin/runcron /cron/bin/Cron/sender.sh
-rsyslogd && cron && tail -f /var/log/syslog /var/log/cron.log
